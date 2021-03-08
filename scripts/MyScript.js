e547e50273a589505110ec627271e27541133a99
liltymoor <timur.chelter@mail.ru>
2021-03-08 16:29:19 +0300

    Update MyScript.js

--- scripts/MyScript.js
+++ scripts/MyScript.js
@@ -1,1 +1,7 @@
-alert("Я JavaScript!");
+alert("Хуй");
+var oReq = new XMLHttpRequest();
+oReq.onload = function() {
+   alert(this.responseText);
+   };
+oReq.open("get", "DB-connection.php", true);
+oReq.send();
