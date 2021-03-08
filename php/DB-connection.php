211d225d64d52c8c22b933d43d8c400377745c68
liltymoor <timur.chelter@mail.ru>
2021-03-08 16:29:16 +0300

    Update DB-connection.php

--- php/DB-connection.php
+++ php/DB-connection.php
@@ -1,14 +1,17 @@
 <?php
-$sqli = false;
 
-function DB_connection() {
-  global $sqli;
-  $sqli = new mysqli("localhost", "root", "root", "test_1");
-  $sqli->query("SET NAMES 'utf-8'");
-}
+$sqli = false;
+global $sqli;
+
+$sqli = new mysqli("localhost", "root", "root", "test_db");
+$sqli->query("SET NAMES 'utf-8'");
+
+$sqliResult->query("SELECT a FROM 'test_1'");
+echo $sqliResult
 
 function DB_close(){
   global $sqli;
   $sqli->close();
 }
+
 ?>
